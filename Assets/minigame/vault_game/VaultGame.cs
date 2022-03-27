d3d5e6b70a4cd79df3ab959574423a29fdc029ab
kmakmichael <m_kmak@u.pacific.edu>
2022-03-26 19:12:38 -0700

    vault game displays the code on paper

--- Assets/minigame/vault_game/VaultGame.cs
+++ Assets/minigame/vault_game/VaultGame.cs
@@ -1,30 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VaultGame : MonoBehaviour
 {
-    private string combo;
-    private string guess;
-    private int difficulty = 5; // start at 1 or 2
+    private int difficulty = 5; // start at 1 or 2, max 15s
+    [SerializeField]
+    private GameObject paper;
+    private Text paper_txt;
+    [SerializeField]
+    private AudioSource paper_sound;
+    [SerializeField]
+    private Text disp_txt;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        paper_txt = paper.transform.GetChild(0).GetComponent<Text>();
+        disp_txt.text = "";
+    }
+
+    void Start() {
         Setup();
+        ShowCode();
     }
 
     private void Setup() {
-        combo = randomCombo(difficulty);
-        guess = "";
+        paper_txt.text = randomCombo(difficulty);
+        disp_txt.text = "";
+    }
+
+    private void ShowCode() {
+        paper.SetActive(true);
+        paper_sound.Play();
+        Invoke("HidePaper", 3);
+    }
+
+    private void HidePaper() {
+        paper_sound.PlayOneShot(paper_sound.clip, 1.0f);
+        paper.SetActive(false);
     }
 
     public void PressButton(string d) {
-        guess += d;
-        Debug.Log("pressed " + d + " digit, guess now " + guess);
+        disp_txt.text += d;
+        Debug.Log("pressed " + d + " digit, guess now " + disp_txt.text);
     }
 
     public void PressEnter() {
-        if (guess.Equals(combo)) {
+        if (disp_txt.text.Equals(paper_txt.text)) {
             Debug.Log("you win");
         } else {
             Debug.Log("you lose");
@@ -32,11 +55,11 @@ public class VaultGame : MonoBehaviour
     }
 
     public void PressDelete() {
-        if (guess.Length > 1) {
-            guess = guess.Substring(0, guess.Length-1);
-            Debug.Log("guess is now " + guess);
+        if (disp_txt.text.Length > 1) {
+            disp_txt.text = disp_txt.text.Substring(0, disp_txt.text.Length-1);
+            Debug.Log("guess is now " + disp_txt.text);
         } else {
-            guess = "";
+            disp_txt.text = "";
             Debug.Log("guess is now empty");
         }
     }
